541d81e6d7296b528555fa7d2201744cb03cb9f4
blackbryant <e49134030e@gmail.com>
2023-09-03 23:34:02 +0800

    新增C#

---
+++ A基本與進階觀念/A基本與進階觀念/類別屬性驗證.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A基本與進階觀念
+{
+    public class 類別屬性驗證
+    {
+        string _id;
+        public string ID 
+        {
+            get { return _id;  }
+            set 
+            {
+                
+                if (value.Length > 15) 
+                {
+                    throw new ArgumentException("不能超過15");
+                }
+                _id = value;
+            }
+        
+        }
+
+        string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+
+                if (value.Length < 8)
+                {
+                    throw new ArgumentException("密碼需要超過10碼");
+                }
+                _password = value;
+            }
+
+        }
+
+        
+
+    }
+}
