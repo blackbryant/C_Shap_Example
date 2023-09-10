8d70b6748770302be341a4b65d52df994516cf48
blackbryant <e49134030e@gmail.com>
2023-09-10 21:55:25 +0800

    OK

--- A基本與進階觀念/A基本觀念/LINQ範例/LINQ範例01.cs
+++ A基本與進階觀念/A基本觀念/LINQ範例/LINQ範例01.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 
 /*
- * 
- * 
- * 
-
- * 
- * 
+ *  LINQ to Object  內存的數據
+ *  LINQ to Sql 
+ *  LINQ to xml 
+ *  LINQ to Excel 
+ *  LINQ to Nosql 
+ *  LINQ to Memcached
+ *  LINQ to Redis
+ *  
  */ 
 namespace A基本觀念.LINQ範例
 {
@@ -28,10 +30,33 @@ namespace A基本觀念.LINQ範例
             workItems.Add(new WorkItem { Id = 3, Desc = "製程D", StartTime = new DateTime(2019, 7, 1, 9, 0, 0), EndTime = new DateTime(2020, 8, 1, 9, 0, 0) });
             workItems.Add(new WorkItem { Id = 3, Desc = "製程E", StartTime = new DateTime(2019, 9, 1, 9, 0, 0), EndTime = new DateTime(2020, 10, 1, 9, 0, 0) });
 
-            //求出加工時間
+            //求出最大加工時間
             var max = workItems.Max(w=>w.EndTime-w.StartTime);
             Console.WriteLine("最常加工時間:{0:%d}天,{0:%h}時,{0:%m}分,{0:%s}秒",max);
 
+            //字串處理
+            string[] str_arr = { "abc", "def" ,"ghij","loklod"};
+            int len = str_arr.Sum(x=> x.Length);
+            Console.WriteLine($"string length:{len}");
+
+            //合併兩個串列
+            List<string> list01 = new List<string> { "a","b","c"};
+            List<string> list02 = new List<string> { "k", "d", "g" };
+            var result = list01.Concat<string>(list02);
+            Console.WriteLine("Join 字串:"+string.Join("、",result));
+
+            //找出消費大於1000的
+            SpendMoney[] spendMoneys =
+            {
+                new SpendMoney(1,10,10),
+                new SpendMoney(2,20,20),
+                new SpendMoney(3,30,30),
+                new SpendMoney(4,40,40),
+                new SpendMoney(5,50,60),
+                new SpendMoney(7,60,10)
+            };
+            int count = spendMoneys.Count(x => (x.Money * x.Amount) > 1000);
+            Console.WriteLine($"超過1000數目有:{count}");
 
         }
 
@@ -47,7 +72,19 @@ namespace A基本觀念.LINQ範例
 
     }
 
+    class SpendMoney
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int Money { get; set; }
 
+        public SpendMoney(int id, int amount, int money)
+        {
+            Id = id;
+            Amount = amount;
+            Money = money;
+        }   
+    }
   
 
 }
